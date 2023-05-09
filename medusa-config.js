7b8c9ed3311bd9cc162137a09f2eefae174f5eb0
Shivam <singhshivamkr@gmail.com>
2023-05-09 20:56:40 +0530

    feat: enable redis

--- medusa-config.js
+++ medusa-config.js
@@ -88,8 +88,7 @@ const projectConfig = {
   database_type: DATABASE_TYPE,
   store_cors: STORE_CORS,
   admin_cors: ADMIN_CORS,
-  // Uncomment the following lines to enable REDIS
-  // redis_url = REDIS_URL
+  redis_url: REDIS_URL,
 };
 
 if (DATABASE_URL && DATABASE_TYPE === "postgres") {
