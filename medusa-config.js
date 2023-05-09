b820592c6341617e40e8e32bee9b95c36effed3f
Shivam <singhshivamkr@gmail.com>
2023-05-09 21:02:30 +0530

    uncomment redis plugins

--- medusa-config.js
+++ medusa-config.js
@@ -66,18 +66,18 @@ const plugins = [
 ];
 
 const modules = {
-  /*eventBus: {
+  eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
-      redisUrl: REDIS_URL
-    }
+      redisUrl: REDIS_URL,
+    },
   },
   cacheService: {
     resolve: "@medusajs/cache-redis",
     options: {
-      redisUrl: REDIS_URL
-    }
-  },*/
+      redisUrl: REDIS_URL,
+    },
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
