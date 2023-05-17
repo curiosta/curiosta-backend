d059b40d0c262842a1596c71408f074da624fa0b
Labham Jain <labhamjain81@gmail.com>
2023-05-17 18:02:47 +0530

    wip?: add console statement to debug subscribers

---
+++ src/subscribers/product.js
@@ -0,0 +1,19 @@
+import { ProductService } from "@medusajs/medusa";
+
+class ProductSubscriber {
+  constructor({ eventBusService }) {
+    eventBusService.subscribe(ProductService.Events.CREATED, async (data) => {
+      console.log('\n\n\n<><><><><>\nProduct Created\n\n\n', data.id)
+    });
+    eventBusService.subscribe(ProductService.Events.UPDATED, async (data) => {
+      console.log('\n\n\n<><><><><>\nProduct Updated\n\n\n', data.id)
+    });
+
+    eventBusService.subscribe(ProductService.Events.DELETED, async (data) => {
+      console.log('\n\n\n<><><><><>\nProduct Deleted\n\n\n', data.id)
+    });
+
+  }
+}
+
+export default ProductSubscriber;
