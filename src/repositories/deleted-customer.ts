c3f7842ad791b99126921deeee4b55c709a866a4
Labham Jain <labhamjain81@gmail.com>
2023-10-06 14:40:03 +0530

    fix: remove unexpected import

--- src/repositories/deleted-customer.ts
+++
@@ -1,7 +0,0 @@
-import {
-  dataSource,
-} from "@medusajs/medusa/dist/loaders/database"
-import { DeletedCustomer } from "../models/deleted-customer"
-
-export const DeletedCustomerRepository = dataSource.getRepository(DeletedCustomer)
-export default DeletedCustomerRepository
