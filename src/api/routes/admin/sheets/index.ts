b4df5ff78f99c1f85ccd09b811588f93aface22d
Labham Jain <labhamjain81@gmail.com>
2023-10-26 15:58:54 +0530

    feat: add google sheet integration

--- src/api/routes/admin/sheets/index.ts
+++ src/api/routes/admin/sheets/index.ts
@@ -1,8 +1,10 @@
-import { ProductCategory, authenticate } from "@medusajs/medusa"
+import { Product, ProductStatus, authenticate } from "@medusajs/medusa"
 import express, { Router } from "express";
 import CategoryService from "../../../../services/category";
-import GoogleSheetAPIService from "../../../../services/google-sheet-api";
+import GoogleSheetAPIService, { ProductData } from "../../../../services/google-sheet-api";
 import ProductService from "../../../../services/product";
+import { CreateProductInput } from "@medusajs/medusa/dist/types/product";
+import { ulid } from "ulid";
 
 export const SheetsRouter = (router: Router) => {
   router.use('/admin/sheets', express.json(), authenticate());
@@ -13,31 +15,53 @@ export const SheetsRouter = (router: Router) => {
     const productService = req.scope.resolve('productService') as ProductService
     try {
       const sheetData = await googleSheetService.getProductDataBySheetId('1TaiFMTqYGirhLrjUkEfCGbV3hCrX9po_tduFw_sETUg');
-
       const promises = sheetData.map(async (entry) => {
         const location = await categoryService.getCategoryByName(entry.Location);
         const category = await categoryService.getCategoryByName(entry.Category);
 
-        const categories: ProductCategory[] = []
+        const categories: { id: string }[] = []
 
         if (location) {
-          categories.push(location)
+          categories.push({ id: location.id })
         }
         if (category) {
-          categories.push(category)
+          categories.push({ id: category.id })
         }
 
         return { ...entry, categories }
       });
 
-      const results = (await Promise.allSettled(promises)).filter(r => r.status === 'fulfilled').map((r: any) => r.value);
+      const results = (await Promise.allSettled(promises)).filter(r => r.status === 'fulfilled').map((r: any) => r.value).map<CreateProductInput & { id: string; rowNumber: number; }>((product: ProductData & { categories: { id: string }[] }) => {
+        return ({
+          id: product["Product ID"],
+          title: product["Product title"],
+          handle: `${product["Product title"].toLowerCase().replace(/-/g, '').replace(/ /g, '-')}_${ulid()}`,
+          description: product.Description,
+          status: (!product.Stocks || product.Draft) ? ProductStatus.DRAFT : ProductStatus.PUBLISHED,
+          categories: product.categories,
+          variants: [
+            {
+              title: 'one size',
+              inventory_quantity: product.Stocks || 0,
+              prices: [
+                {
+                  amount: 10000,
+                  currency_code: 'usd'
+                }
+              ]
+            }
+          ],
 
-      console.log('results: ', results);
+          rowNumber: product.rowNumber
+        }) as CreateProductInput & { id: string; rowNumber: number; }
+      });
 
       const bulkAddResult = await productService.addBulkProducts(results)
-      console.log('bulk results:', bulkAddResult);
+      const savedProductIdAndRowNumber = bulkAddResult.saved.map((p) => ({ id: p.id, rowNumber: p.rowNumber }))
 
-      return res.json({ status: 'ok' })
+      const affectedCellsCount = await googleSheetService.updateProductId('1TaiFMTqYGirhLrjUkEfCGbV3hCrX9po_tduFw_sETUg', savedProductIdAndRowNumber)
+
+      return res.json({ status: 'ok', affectedCellsCount })
     } catch (error) {
       return res.status(500).json({ status: 500, message: 'An error occurred!', error: error instanceof Error ? error.message : error })
     }
